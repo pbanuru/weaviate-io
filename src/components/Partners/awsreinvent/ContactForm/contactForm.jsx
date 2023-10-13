7b82e59d115328f21517081d0fa8bcf034ab8c94
Shan-Weaviate <shan@weaviate.io>
2023-10-13 15:42:25 +0200

    Update contactForm.jsx

--- src/components/Partners/awsreinvent/ContactForm/contactForm.jsx
+++ src/components/Partners/awsreinvent/ContactForm/contactForm.jsx
@@ -5,7 +5,7 @@ import Link from '@docusaurus/Link';
 export default function ContactForm() {
   useEffect(() => {
     const script = document.createElement('script');
-    script.src = '//js-eu1.hsforms.net/forms/embed/v2.js';
+    script.src = '//js.hsforms.net/forms/embed/v2.js';
     document.body.appendChild(script);
 
     script.addEventListener('load', () => {
@@ -13,8 +13,9 @@ export default function ContactForm() {
       if (window.hbspt) {
         // @TS-ignore
         window.hbspt.forms.create({
-          portalId: '143457533',
-          formId: '9f950fe9-da45-40dd-87fc-cfebf4b1a5e2',
+          region: 'na1',
+          portalId: '8738733',
+          formId: 'bc7699b1-28ad-4a9b-a66f-3926bcb18d81',
           target: '#hubspotForm',
         });
       }
@@ -28,42 +29,6 @@ export default function ContactForm() {
           <div className={styles.contactSection}>
             <h2 className={styles.title}>Book a meeting with the team</h2>
             <div id="hubspotForm"></div>
-            {/*  <form
-              className={styles.formContact}
-              action="https://formcarry.com/s/FZwYB5WwuG"
-              method="POST"
-            >
-              <h2 className={styles.title}>Contact us </h2>
-              <div className={styles.links}>
-                <p>
-                  Do you have any questions? Let’s get this conversation
-                  started.
-                </p>
-              </div>
-
-              <input
-                type="input"
-                name="formName"
-                placeholder="First Name"
-              ></input>
-              <span></span>
-              <input
-                type="email"
-                name="formEmail"
-                placeholder="Work Email Address"
-              ></input>
-              <span></span>
-
-              <textarea
-                type="text"
-                name="formMessage"
-                className={styles.formMessage}
-                placeholder="Message"
-              ></textarea>
-              <button type="submit" name="formSumbit">
-                Send
-              </button>
-            </form> */}
           </div>
         </div>
       </div>
